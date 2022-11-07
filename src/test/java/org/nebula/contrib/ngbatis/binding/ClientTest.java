622e2ac11b53ba3595001b8dec66b323d53a09c8
Nicole00 <anqi.wang@vesoft.com>
2022-11-07 10:03:09 +0800

    add snapshot deploy action

---
+++ src/test/java/org/nebula/contrib/ngbatis/binding/ClientTest.java
@@ -0,0 +1,7 @@
+/* Copyright (c) 2022 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License.
+ */
+ 
+package org.nebula.contrib.ngbatis.binding;public class ClientTest {
+}
