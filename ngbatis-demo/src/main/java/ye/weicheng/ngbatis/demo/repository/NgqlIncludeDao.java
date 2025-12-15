9cd65486a34fad16390e5d5c54e63ef28576f1e4
CorvusYe <CorvusY@foxmail.com>
2025-12-15 11:07:56 +0800

    fix: <nGQL> fails to function properly when using `ng.include` across multiple lines.

--- ngbatis-demo/src/main/java/ye/weicheng/ngbatis/demo/repository/NgqlIncludeDao.java
+++ ngbatis-demo/src/main/java/ye/weicheng/ngbatis/demo/repository/NgqlIncludeDao.java
@@ -1,5 +1,8 @@
 package ye.weicheng.ngbatis.demo.repository;
 
+import java.util.List;
+import java.util.Map;
+import org.nebula.contrib.ngbatis.models.data.NgTriplet;
 import org.springframework.data.repository.query.Param;
 import ye.weicheng.ngbatis.demo.pojo.Person;
 
@@ -12,4 +15,6 @@ public interface NgqlIncludeDao {
   Integer testInclude(@Param("myInt") Integer myInt);
 
   Integer returnAge(@Param("person")Person person);
+  
+  List<Map> selectByFilter(@Param("person") Person person);
 }
--- ngbatis-demo/src/test/java/ye/weicheng/ngbatis/demo/repository/NgqlIncludeTest.java
+++ ngbatis-demo/src/test/java/ye/weicheng/ngbatis/demo/repository/NgqlIncludeTest.java
@@ -1,6 +1,10 @@
 package ye.weicheng.ngbatis.demo.repository;
 
+import com.alibaba.fastjson.JSON;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.nebula.contrib.ngbatis.models.data.NgTriplet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ye.weicheng.ngbatis.demo.pojo.Person;
@@ -25,6 +29,16 @@ public class NgqlIncludeTest {
     System.out.println("nGQL引用额外参数测试:" + ngqlIncludeDao.returnAge(person));
     System.out.println("nGQL跨mapper引用测试:" + ngqlInclude4diffMapperDao.testInclude(1));
 
+  }
+  
+  @Test
+  public void testFilter() {
+    Person person = new Person();
+    List<Map> ngTriplet = ngqlIncludeDao.selectByFilter(person);
+    System.out.println(JSON.toJSONString(ngTriplet));
+    person.setName("赵小洋");
+    ngTriplet = ngqlIncludeDao.selectByFilter(person);
+    System.out.println(JSON.toJSONString(ngTriplet));
   }
 
 }
